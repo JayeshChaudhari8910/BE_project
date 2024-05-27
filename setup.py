f02b135991c628031dd635a9773e6ef6c94d9c45
JayeshChaudhari8910 <117355587+JayeshChaudhari8910@users.noreply.github.com>
2024-05-27 07:38:28 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='your_project_name',
+    version='0.1',
+    install_requires=[
+        'setuptools>=58.0.0',
+        'wheel'
+    ]
+)
